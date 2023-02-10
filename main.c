ea05160ba948d3aa2c9183790b3865a1c2625a78
HansSorongon <hanssorongon812@gmail.com>
2023-02-10 21:45:37 +0800

    refactoring, UI changes

--- main.c
+++ main.c
@@ -17,6 +17,10 @@
 //
 // set selected = 0 if it brings you somewhere else.
 
+void display_menu();
+
+typedef char string[50];
+
 // main structure for questions
 typedef struct {
   char topic[20];
@@ -29,30 +33,34 @@ typedef struct {
 } question_t;
 
 
-void delay(int seconds) {
-    int ms = 1000 * seconds;
+void delay(float seconds) {
+    float ms = 1000 * seconds;
     clock_t start_time = clock();
     while (clock() < start_time + ms); // run a while loop for seconds seconds.
 }
 
-int display_options(char options[][20], size_t num_options) {
+// returns selected
+int display_options(char prompt[60], char options[][20], size_t num_options) {
+
 
   int select = 0;
   int selected = 0;
-
-  // initial menu
-  /* for (int i = 0; i < num_options; i++) { */
-  /*   printf("%c - %s\n", (select == i) ? '>' : ' ', options[i]); */
-  /* } */
 
   // clear loop
   while (!selected) {
 
     system("cls");
 
+    printf("%s", prompt);
+
     for (int i = 0; i < num_options; i++) {
-      printf("%c - %s\n", (select == i) ? '>' : ' ', options[i]);
+      printf("\t  %c - %s\n", (select == i) ? '>' : ' ', options[i]);
     }
+
+    printf("\n\t-----------------------\n\n");
+    printf("\n [k] up");
+    printf("\n [j] down");
+    printf("\n [d] select\n\n");
 
     switch(getch()) {
       case 'j': case 'J':
@@ -68,8 +76,8 @@ int display_options(char options[][20], size_t num_options) {
       case 'd': case 'D':
         selected = 1;
         break;
-    }
-}
+      }
+  }
 
   return select;
 }
@@ -130,78 +138,93 @@ void import_data() {
 
 }
 
-void manage_data() {
+void manage_data(question_t *questions) {
 
   enum {
-    ADD=1,
-    EDIT=2,
-    DELETE=3,
-    IMPORT=4,
-    EXPORT=5,
-    BACK=6
+    ADD=0,
+    EDIT=1,
+    DELETE=2,
+    IMPORT=3,
+    EXPORT=4,
+    BACK=5
   };
 
   int logged_in = prompt_password();
 
   if (logged_in) {
-    // run data management while loop
 
-    int select = 1;
-    int selected = 0;
+    char manage_options[6][20] = {"Add", "Edit", "Delete", "Import", "Export", "Back"};
+    int select = display_options("   Welcome admin, what would you like to do?\n\n", manage_options, 6);
+
+    switch(select) {
+      case ADD:
+        break;
+      case EDIT:
+        break;
+      case DELETE:
+        break;
+      case IMPORT:
+
+        system("cls");
+
+        string file_name;
+        strcpy(file_name, "test.txt");
+        printf("\n Importing %s \n", file_name);
+        printf(" ");
+
+        // IMPORT LOGIC
+        FILE* fptr;
+
+        fptr = fopen(file_name, "r");
+        char str[150];
+
+        if (fptr != NULL) {
+          while (fgets(str, 150, fptr)) {
+            printf("%s", str);
 
 
-    while (!selected) {
-      system("cls");
-      printf("Welcome admin.\n\n") ;
-      printf("%c - Add a record\n", (select == 1) ? '>' : ' ') ;
-      printf("%c - Edit a record\n", (select == 2) ? '>' : ' ') ;
-      printf("%c - Delete a record\n", (select == 3) ? '>' : ' ') ;
-      printf("%c - Import data\n", (select == 4) ? '>' : ' ') ;
-      printf("%c - Export data\n", (select == 5) ? '>' : ' ') ;
-      printf("%c - Back\n\n", (select == 6) ? '>' : ' ') ;
 
 
-      handle_cursor(&select, 6, &selected);
-
-      if (selected) {
-        switch (select) {
-          case ADD:
-            break;
-          case EDIT:
-            break;
-          case DELETE:
-            break;
-          case IMPORT:
-            import_data();
-            break;
-          case EXPORT:
-            break;
-          case BACK:
-            break;
+          }
+        } else {
+          printf("File not found. Terminating...\n\n");
         }
-      }
+
+        // store to struct array here!
+
+        if (fptr) {
+          for (int i = 0; i < 12; i++) {
+            printf("#");
+            delay(0.2);
+          }
+          printf(" DONE!");
+        }
+
+        fclose(fptr);
+        printf("\n\n Press any key to continue...");
+
+        getch();
+
+        break;
+      case EXPORT:
+        break;
+      case BACK:
+        display_menu();
+        break;
     }
+
   } else {
 
-    int select = 1; // this is in scope of while that runs within else
-    int selected = 0;
+    char try_options[2][20] = { "Try Again", "Back" };
+    int select = display_options("   You entered the wrong password!\n\n", try_options, 2);
 
-    while (!selected) {
-      system("cls");
-      printf("You entered the wrong password!\n\n") ;
-      printf("%c - Try again\n", (select == 1) ? '>' : ' ') ;
-      printf("%c - Back", (select == 2) ? '>' : ' ') ;
-      handle_cursor(&select, 2, &selected);
-
-      if (selected) {
-        switch(select) {
-          case 1:
-            manage_data(); // Recursive
-            break;
-          case 2:
-            break;
-        }
-      }
+    switch(select) {
+      case 0:
+        manage_data(questions);
+        break;
+      case 1:
+        display_menu();
+        break;
     }
   }
 }
@@ -289,60 +312,31 @@ void play_menu() {
 void display_menu() {
 
   enum {
-    PLAY=1,
-    MANAGE=2,
-    EXIT=3
+    PLAY=0,
+    MANAGE=1,
+    EXIT=2
   };
 
-  int select = 1;
-  int selected = 0;
-  int end = 0;
+  char menu_options[3][20] = {"Play", "Manage Data", "Exit"};
+  int selected = display_options("\n\t----- QUIZ MASTER -----\n\n", menu_options, 3);
 
-  while (!end) {
+  question_t questions[100] // QUESTIONS STORE HERE AFTER LOAD
 
-    printf("\n\t------- Quiz Game! -------\n\n");
-    printf("\t    %c  - Play\n", (select == 1) ? '>' : ' ');
-    printf("\t    %c  - Manage Data\n", (select == 2) ? '>' : ' ');
-    printf("\t    %c  - Exit\n", (select == 3) ? '>' : ' ');
-    printf("\n\t--------------------------\n\n");
-
-    handle_cursor(&select, 3, &selected);
-    if (selected) {
-      switch (select) {
-        case PLAY:
-          selected = 0; // reset selected because we want to go back here
-                        // later.
-          play_menu();
-          break;
-        case MANAGE:
-          selected = 0;
-          manage_data();
-          break;
-        case EXIT:
-          printf("\n\t   See you again soon!");
-          delay(1); // delay clear screen
-          end = 1;
-          break;
-      }
-    }
-
-    system("cls");
+  switch(selected) {
+    case PLAY:
+      break;
+    case MANAGE:
+      manage_data(questions);
+      break;
+    case EXIT:
+      break;
   }
+
 }
 
 int main(int argc, char **argv) {
 
-  /* display_menu(1); */
-
-  int num;
-  scanf("%d", &num);
-  char options[num][20];
-
-  for (int i = 0; i < num; i++) {
-    strcpy(options[i], "Test Arbitrary");
-  }
-
-  display_options(options, sizeof(options) / sizeof(options[0]));
+  display_menu();
 
   return 0;
 }
