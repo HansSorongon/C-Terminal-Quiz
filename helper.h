ddbb51c3ee031b2ae1876eb9f87dd4c75d0339e9
HansSorongon <hanssorongonn@gmail.com>
2023-04-07 23:22:12 +0800

    finalize

--- helper.h
+++ helper.h
@@ -19,14 +19,14 @@ typedef struct Question question_t;
 // struct for file properties, list of questions, size, and file name.
 struct File
 {
-  question_t questions[100];
-  size_t size;
-  string30_t file_name;
+  question_t questions[100]; // questions array
+  size_t size; // current size of the data
+  string30_t file_name; // file name for use within program
 };
 
 typedef struct File file_t;
 
-// FUNCTIONS
+// FUNCTIONS | Descriptions in helper.c file.
 
 void delay(float);
 
